3c6b390781753947fed7dc80056542d3efd6be49
paul <paul@91a82408-f9f9-11e0-87e5-adb29e31ef75>
2011-11-24 04:20:16 +0000

    committing changes for the management and profile editor for openid-server-app
    git-svn-id: http://svn.lockaby.org/public/openid-server-app/trunk@12 91a82408-f9f9-11e0-87e5-adb29e31ef75

--- perl-lib/org/lockaby/id/handler/profile.pm
+++ perl-lib/org/lockaby/id/handler/profile.pm
@@ -52,8 +52,19 @@ sub get_profile {
     # figure out who we are
     my $username = $engine->get_username();
 
+    my $details_sth = $dbh->prepare_cached(q|
+        SELECT email_address, nickname, fullname, is_manager
+        FROM users
+        WHERE username = LOWER(?)
+    |);
+    $details_sth->execute($username);
+    my ($email_address, $nickname, $fullname, $is_manager) = $details_sth->fetchrow();
+    $details_sth->finish();
+
     my $submit = $q->param("submit");
     if (defined($submit)) {
+        my $form = $q->param('form');
+
         my $content = "";
         my @errors = ();
 
@@ -61,20 +72,94 @@ sub get_profile {
         local $dbh->{RaiseError} = 0;
 
         eval {
-            my $remove = [$q->param('remove[]')];
-            foreach my $item (@{$remove}) {
-                my $remove_sth = $dbh->prepare_cached(q|
-                    UPDATE trusted
-                    SET authorized = 0
-                    WHERE id = ? AND user_id IN (
-                        SELECT user_id FROM users WHERE username = LOWER(?)
-                    )
-                |);
-                $remove_sth->execute($item, $username);
-                $remove_sth->finish();
+            if (defined($form) && $form eq "trusted") {
+                my $remove = [$q->param('remove[]')];
+                foreach my $item (@{$remove}) {
+                    my $remove_sth = $dbh->prepare_cached(q|
+                        UPDATE trusted
+                        SET authorized = 0
+                        WHERE id = ? AND user_id IN (
+                            SELECT user_id FROM users WHERE username = LOWER(?)
+                        )
+                    |);
+                    $remove_sth->execute($item, $username);
+                    $remove_sth->finish();
+                }
+
+                $content = 'success';
             }
 
-            $content = 'success';
+            if (defined($form) && $form eq "profile") {
+                my $email_address = $q->param('email_address');
+                my $fullname = $q->param('fullname');
+                my $nickname = $q->param('nickname');
+
+                my $save_sth = $dbh->prepare_cached(q|
+                    UPDATE users
+                    SET email_address = ?,
+                        fullname = ?,
+                        nickname = ?
+                    WHERE username = LOWER(?)
+                |);
+                $save_sth->execute($email_address, $fullname, $nickname, $username);
+                $save_sth->finish();
+            }
+
+            if (defined($form) && $form eq "management" && $is_manager) {
+                my $action = $q->param('action');
+
+                if (defined($action) && $action eq "clear") {
+                    my $type = $q->param('type');
+
+                    if (defined($type)) {
+                        if ($type eq "sessions") {
+                            my $clear_sth = $dbh->prepare("DELETE FROM sessions");
+                            $clear_sth->execute();
+                            $content = 'success';
+                        }
+                        if ($type eq "autologin") {
+                            my $clear_sth = $dbh->prepare("DELETE FROM autologin");
+                            $clear_sth->execute();
+                            $content = 'success';
+                        }
+                        if ($type eq "secrets") {
+                            my $clear_sth = $dbh->prepare("DELETE FROM secrets");
+                            $clear_sth->execute();
+                            $content = 'success';
+                        }
+                    }
+                }
+
+                if (defined($action) && $action eq "toggle") {
+                    my $type = $q->param('type');
+                    my $value = $q->param('value');
+                    my $actor = $q->param('username');
+
+                    if (defined($type) && defined($actor)) {
+                        if ($type eq "is_manager") {
+                            my $toggle_sth = $dbh->prepare("UPDATE users SET is_manager = ? WHERE username = LOWER(?)");
+                            if ($value) {
+                                $toggle_sth->execute(1, $actor);
+                            } else {
+                                $toggle_sth->execute(0, $actor);
+                            }
+                            $toggle_sth->finish();
+                            $content = 'success';
+                        }
+
+                        if ($type eq "is_enabled") {
+                            my $toggle_sth = $dbh->prepare("UPDATE users SET is_enabled = ? WHERE username = LOWER(?)");
+                            if ($value) {
+                                $toggle_sth->execute(1, $actor);
+                            } else {
+                                $toggle_sth->execute(0, $actor);
+                            }
+                            $toggle_sth->finish();
+                            $content = 'success';
+                        }
+                    }
+                }
+            }
 
             $dbh->commit();
         };
@@ -148,6 +233,86 @@ sub get_profile {
         |);
     }
 
+    my $tools_tab_link = "";
+    my $tools_tab_content = "";
+
+    if ($is_manager) {
+        my $count_sessions_sth = $dbh->prepare_cached("SELECT COUNT(*) FROM sessions");
+        $count_sessions_sth->execute();
+        my ($count_sessions) = $count_sessions_sth->fetchrow();
+        $count_sessions_sth->finish();
+
+        my $count_autologin_sth = $dbh->prepare_cached("SELECT COUNT(*) FROM autologin");
+        $count_autologin_sth->execute();
+        my ($count_autologin) = $count_autologin_sth->fetchrow();
+        $count_autologin_sth->finish();
+
+        my $count_secrets_sth = $dbh->prepare_cached("SELECT COUNT(*) FROM secrets");
+        $count_secrets_sth->execute();
+        my ($count_secrets) = $count_secrets_sth->fetchrow();
+        $count_secrets_sth->finish();
+
+        my @users = ();
+        my $all_users_sth = $dbh->prepare_cached(q|
+            SELECT id, username, is_manager, is_enabled
+            FROM users
+            ORDER BY username ASC
+        |);
+        $all_users_sth->execute();
+        while (my ($all_users_id, $all_users_username, $all_users_is_manager, $all_users_is_enabled) = $all_users_sth->fetchrow()) {
+            my $is_enabled_checkbox = "";
+            $is_enabled_checkbox = q|checked="checked"| if ($all_users_is_enabled);
+            my $is_manager_checkbox = "";
+            $is_manager_checkbox = q|checked="checked"| if ($all_users_is_manager);
+
+            push(@users, qq|
+                <div class="row">
+                    <div class="username">${all_users_username}</div>
+                    <div class="is">
+                        <input type="checkbox" name="is_manager" ${is_manager_checkbox}/>
+                    </div>
+                    <div class="is">
+                        <input type="checkbox" name="is_enabled" ${is_enabled_checkbox}/>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            |);
+        }
+        $all_users_sth->finish();
+
+        $tools_tab_link = q|
+            <li><a href="#management">Management</a></li>
+        |;
+        $tools_tab_content = qq|
+            <div id="management">
+                <ul>
+                    <li>
+                        <a href="javascript:openid.management.clear('sessions');">Clear all sessions.</a>
+                          - <span class="count_sessions">${count_sessions}</span> active sessions. THIS WILL END YOUR SESSION, TOO!<br/>
+                    </li>
+                    <li>
+                        <a href="javascript:openid.management.clear('autologin');">Clear all automatic logins.</a>
+                          - <span class="count_autologin">${count_autologin}</span> automatic login keys.<br/>
+                    </li>
+                    <li>
+                        <a href="javascript:openid.management.clear('secrets');">Clear all OpenID secrets.</a>
+                          - <span class="count_secrets">${count_secrets}</span> OpenID secrets.<br/>
+                    </li>
+                </ul>
+
+                <form autocomplete="off" method="POST">
+                    <div class="header">
+                        <div class="username">Username</div>
+                        <div class="is">Is manager?</div>
+                        <div class="is">Is enabled?</div>
+                        <div class="clear"></div>
+                    </div>
+                    ${\join("", @users)}
+                </form>
+            </div>
+        |;
+    }
+
     my $t = org::lockaby::id::template->new({
         title => $config->{url} . " - profile",
     });
@@ -155,33 +320,93 @@ sub get_profile {
     $r->content_type('text/html; charset=utf-8');
     print $t->get_header();
     print qq|
-        <form autocomplete="off" method="POST">
-            <div id="title">
-                ${\$config->{url}} profile
+        <div id="title">
+            ${\$config->{url}} profile
+        </div>
+        <div id="header">
+            <div class="username">Logged in as: <b>${username}</b></div>
+            <div class="logout">[<a href="/openid/logout">logout</a>]</div>
+            <div class="clear"></div>
+        </div>
+        <div id="tabs">
+            <ul>
+                <li><a href="#trusted">Trusted Sites</a></li>
+                <li><a href="#profile">My Profile</a></li>
+                ${tools_tab_link}
+            </ul>
+            <div id="trusted">
+                <form autocomplete="off" method="POST">
+                    <div class="introduction">
+                        Configure the list of sites that you are authorizing to use your ID.
+                    </div>
+
+                    <div class="select">
+                        Select: <a href="javascript:void(0);" onclick="openid.trusted.select(this, 'all');">all</a> \|
+                                <a href="javascript:void(0);" onclick="openid.trusted.select(this, 'none');">none</a>
+                    </div>
+                    <div class="header">
+                        <div class="name">Site</div>
+                        <div class="clear"></div>
+                    </div>
+                    ${\join("\n", @trusted)}
+                </form>
             </div>
             <div id="profile">
-                <div class="username">Logged in as: <b>${username}</b></div>
-                <div class="logout">[<a href="/openid/logout">logout</a>]</div>
-                <div class="clear"></div>
+                <form autocomplete="off" method="POST">
+                    <div class="introduction">
+                        These fields will be sent to sites when you log in. For more details, read about the
+                        <a href="http://openid.net/specs/openid-simple-registration-extension-1_0.html">OpenID
+                        Simple Registration Extension</a>.
+                    </div>
 
-                <div style="margin: 4px 0px; text-align: center;">Configure the list of sites that you are authorizing to use your ID.</div>
-                Select: <a href="javascript:void(0);" onclick="openid.select(this, 'all');">all</a> \|
-                        <a href="javascript:void(0);" onclick="openid.select(this, 'none');">none</a>
-                <br/>
-                <div class="header">
-                    <div class="name">Site</div>
-                    <div class="clear"></div>
-                </div>
-                ${\join("\n", @trusted)}<br/>
+                    <div class="row">
+                        <div class="label">Email Address:</div>
+                        <div class="value">
+                            <input type="text" value="${email_address}" name="email_address"/>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+
+                    <div class="row">
+                        <div class="label">Full Name:</div>
+                        <div class="value">
+                            <input type="text" value="${fullname}" name="fullname"/>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+
+                    <div class="row">
+                        <div class="label">Nickname:</div>
+                        <div class="value">
+                            <input type="text" value="${nickname}" name="nickname"/>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+
+                    <div style="text-align: center;">
+                        <input type="submit" name="save" value="save"/>
+                    </div>
+                </form>
             </div>
-        </form>
+            ${tools_tab_content}
+        </div>
         <script type="text/javascript">
             jQuery(document).ready(function() {
-                jQuery('#profile').closest('form').submit(function (event) {
-                    openid.events.remove(event, this);
+                jQuery('#tabs').tabs();
+                jQuery('#management form').submit(function (event) {
+                    event.preventDefault();
                 });
-                jQuery('#profile').closest('form').find('input[type="checkbox"]').click(function (event) {
-                    openid.checked(event, this);
+                jQuery('#management form input[type="checkbox"]').change(function (event) {
+                    openid.management.save(event, this);
+                });
+                jQuery('#profile form').submit(function (event) {
+                    openid.profile.save(event, this);
+                });
+                jQuery('#trusted form').submit(function (event) {
+                    openid.trusted.remove(event, this);
+                });
+                jQuery('#trusted form').find('input[type="checkbox"]').click(function (event) {
+                    openid.trusted.checked(event, this);
                 });
             });
         </script>
